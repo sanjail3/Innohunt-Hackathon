2a0f2051c4e68b451b27e2afcfad64c756b177f0
mdabucse <131733014+mdabucse@users.noreply.github.com>
2024-06-16 16:59:40 +0530

    Constants Added

---
+++ frontend/constants/index.ts
@@ -0,0 +1,43 @@
+
+export const navLinks = [
+    {
+      label: "Home",
+      route: "/",
+      icon: "home.svg",
+    },
+    {
+      label: "Image Restore",
+      route: "/transformations/add/restore",
+      icon: "menu.svg",
+    },
+    {
+      label: "Generative Fill",
+      route: "/transformations/add/fill",
+      icon: "stars.svg",
+    },
+    {
+      label: "Object Remove",
+      route: "/transformations/add/remove",
+      icon: "filler.svg",
+    },
+    {
+      label: "Object Recolor",
+      route: "/transformations/add/recolor",
+      icon: "camera.svg",
+    },
+    {
+      label: "Background Remove",
+      route: "/transformations/add/removeBackground",
+      icon: "/assets/icons/camera.svg",
+    },
+    {
+      label: "Profile",
+      route: "/profile",
+      icon: "/assets/icons/profile.svg",
+    },
+    {
+      label: "Buy Credits",
+      route: "/credits",
+      icon: "/assets/icons/bag.svg",
+    },
+  ];
